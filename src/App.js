2097cc17047ebb666af500e6f130e9a26229b060
QuietMonkey <taillandierbertrand@gmail.com>
2019-08-23 01:32:31 +0200

    axios popular & create station component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
+import Station from './Station';
 
 class App extends Component {
+  state = {
+    data:[]}
+
+  getPopular = () => {
+    axios.get('https://api.comparatrip.eu/cities/popular/7 ')
+      .then((response) => {
+        this.setState({data : response.data});
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  componentDidMount(){
+    this.getPopular()
+  }
+
+
   render() {
+    console.log(this.state)
     return (
+
       <div className="App">
         <div className='card left'>
           <h2>Quel est votre trajet?</h2>
@@ -14,7 +36,7 @@ class App extends Component {
           <input className='inputSearch dateEnd'></input>
         </div>
         <div className='card right'>
-
+          <Station name='Paris'/>
         </div>
       </div>
     );
---
+++ src/Station.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Station = ({name}) => {
+    return(
+        <h3 className='station'>{name}</h3>
+    )
+}
+
+export default Station
